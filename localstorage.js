62d043475d22f7e32539ab42d1f01243b7899b8b
Mario Cortes <mcortes.1113@gmail.com>
2019-11-22 00:36:38 -0600

    New file created to host local storage functions

---
+++ localstorage.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    var searchHistory = [,,,,];
+    console.log("search history size" + searchHistory.length);
+
+    //function to remove last array element using pop and add new element using unshift
+}
