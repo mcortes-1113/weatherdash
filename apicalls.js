bbb389c82e0c92e1240f00e80c05e7706f20770c
Mario Cortes <mcortes.1113@gmail.com>
2019-11-23 02:31:30 -0600

    moved apiUV call to run with currentAPI. Replaced hard code values for lat and lon with variables populated by currentAPI response.

--- apicalls.js
+++ apicalls.js
@@ -9,18 +9,19 @@ $(document).ready(function(){
     var apiCity = "nashville"; //replace with city name from user input
     var apiCountry = ",US";
     var apiKey = "&appid=f63825a9b274e4cb840e2e60d64d9e3c";
+    var apiUnits = "&units=imperial";
     
-    var apiCurrentURL = proxy + apiCurrentBaseString + apiCity + apiCountry + apiKey;
-    var apiFCastURL = proxy + apiFCastBaseString + apiCity + apiCountry + apiKey;
+    var apiCurrentURL = proxy + apiCurrentBaseString + apiCity + apiCountry + apiKey + apiUnits;
+    var apiFCastURL = proxy + apiFCastBaseString + apiCity + apiCountry + apiKey + apiUnits;
 
     var apiIconsBaseString = "http://openweathermap.org/img/wn/";
     var apiIconCode = "10d"; //replace with icon code from api responses
     var apiIconURL = apiIconsBaseString + apiIconCode + ".png"; //may need to add @2x before .png
 
     var apiUVBaseString = "http://api.openweathermap.org/data/2.5/uvi?";
-    var apiUVlat = "lat=" + "36.17"; // need to replace with variable populated by api response 36.174465
-    var apiUVlon = "&lon=" + "-86.77"; // need to replace with variable populated by api response -86.767960
-    var apiUVURL = apiUVBaseString + apiUVlat + apiUVlon + apiKey;
+    // var apiUVlat = "lat=" + "36.17"; // need to replace with variable populated by api response 36.174465
+    // var apiUVlon = "&lon=" + "-86.77"; // need to replace with variable populated by api response -86.767960
+    // var apiUVURL = apiUVBaseString + apiUVlat + apiUVlon + apiKey;
 
     //api calls
 
@@ -29,36 +30,57 @@ $(document).ready(function(){
         $.ajax({
             url: apiCurrentURL,
             method: "GET",
-            dataType: "JSON",
-            // headers: {
-            //     //had to add in headers to retrieve information. ***research***
-            //     "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-            //     "x-rapidapi-key": "f63825a9b274e4cb840e2e60d64d9e3c",
-            //     }
         }).then(function(apiCurrentResponse) {
-        console.log(apiCurrentResponse);
+            getUV(apiCurrentResponse);
+            createCurrentElements(apiCurrentResponse);
+            // console.log('city: ' + apiCurrentResponse.name);
+            // console.log('temp: ' + apiCurrentResponse.main.temp + 'F');
+            // console.log('humidity: ' +apiCurrentResponse.main.humidity + '%');
+            // console.log('wind speed: ' + apiCurrentResponse.wind.speed + 'mph');
+            // console.log('lat: ' + apiCurrentResponse.coord.lat);
+            // console.log('lon: ' + apiCurrentResponse.coord.lon);
+            console.log(apiCurrentResponse);
         });
     });
+
+        //UV
+        function getUV(apiCurrentResponse){
+            var apiUVlat = 'lat=' + apiCurrentResponse.coord.lat;
+            var apiUVlon = 'lon=' + apiCurrentResponse.coord.lon;
+            var apiUVURL = apiUVBaseString + apiUVlat + '&' + apiUVlon + apiKey;
+
+            $.ajax({
+                url: apiUVURL,
+                method: "GET",
+            }).then(function(apiUVResponse) {
+                UV = 'UV index: ' + apiUVResponse.value
+                console.log(UV);
+                console.log(apiUVResponse.value);
+                console.log(apiUVResponse);
+            });
+        };
 
     //forecast
     $("#fcasttestbtn").on("click", function(event) {
         $.ajax({
             url: apiFCastURL,
-            method: "GET",
+            method: "GET", 
             dataType: "JSON",
         }).then(function(apiFCastResponse) {
         console.log(apiFCastResponse);
         });
     });
 
-    //UV
-    $("#uvtestbtn").on("click", function(event) {
-        $.ajax({
-            url: apiUVURL,
-            method: "GET",
-        }).then(function(apiUVResponse) {
-        console.log(apiUVResponse);
-        });
-    });
+    // * Include a 5-Day Forecast below the current weather conditions. Each day for the 5-Day Forecast should display the following:
+
+    // * Date
+  
+    // * Icon image (visual representation of weather conditions)
+  
+    // * Temperature
+  
+    // * Humidity
+
+
 
 });
