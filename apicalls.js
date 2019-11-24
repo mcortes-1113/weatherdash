5ccfcc971d9b99392e4175379d45a30533a0d959
Mario Cortes <mcortes.1113@gmail.com>
2019-11-24 00:17:53 -0600

    added code to retrieve current geolocation

--- apicalls.js
+++ apicalls.js
@@ -1,5 +1,15 @@
 
+
+    // navigator.geolocation.getCurrentPosition(function(position) {
+    //     console.log(position.coords.latitude);
+    //     console.log(position.coords.longitude);
+    //   });
+
     //api variables
+
+
+
+
 
     var proxy = "https://cors-anywhere.herokuapp.com/";
 
