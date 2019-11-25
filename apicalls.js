5626d61c856303b2380db46b3079b3ac41409da0
Mario Cortes <mcortes.1113@gmail.com>
2019-11-25 17:59:15 -0600

    code cleanup

--- apicalls.js
+++ apicalls.js
@@ -25,85 +25,6 @@ var apiUVlon;
 var apiUVURL;
 var UV;
 
-
-// var currentLat;
-// var currentLon;
-// var apiLocationURL;
-
-// //Get current location and assign coordinate values to variable
-// navigator.geolocation.getCurrentPosition(function(position) {
-// currentLat = position.coords.latitude;
-// currentLon = position.coords.longitude;
-// apiLocationURL = proxy + apiLocationBaseString + "lat=" + currentLat + "&lon=" + currentLon;
-// console.log(currentLat);
-// console.log(currentLon);
-// console.log(apiLocationURL);
-// getCurrentLocationData(event);
-// });
-// function getCurrentLocationData(event) {
-//     console.log(currentLat);
-//     console.log(currentLon);
-//     console.log(apiLocationURL);
-//     $.ajax({
-//         url: apiLocationURL,
-//         method: "GET",
-//     }).then(function(apiLocationResponse) {
-//         var currentCity = apiLocationResponse.name;
-//         var currentTemp = apiLocationResponse.main.temp + " F";
-//         var currentIconCode = apiLocationResponse.weather.icon
-//         var currentIconSource = apiIconsBaseString + currentIconCode + ".png";
-//         var locationIconImageEl = "<img src=" + currentIconSource + ">"
-//         $("#currentLoc").textContent(currentCity);
-//         $("#currentTemp").textContent(currentTemp);
-//         $("#locationIcon").prepend(locationIconImageEl);
-
-//         // getUV(apiLocationResponse);
-//         // createCurrentElements(apiCurrentResponse);
-
-//         console.log(apiLocationResponse);
-//     });
-// };
-
-    //current weather
-
-    // function getCurrentWeather(event) {
-    //     $.ajax({
-    //         url: apiCurrentURL,
-    //         method: "GET",
-    //     }).then(function(apiCurrentResponse) {
-    //         // getUV(apiCurrentResponse);
-    //         // createCurrentElements(apiCurrentResponse);
-    //         console.log('city: ' + apiCurrentResponse.name);
-    //         console.log('temp: ' + apiCurrentResponse.main.temp + 'F');
-    //         console.log('humidity: ' +apiCurrentResponse.main.humidity + '%');
-    //         console.log('wind speed: ' + apiCurrentResponse.wind.speed + 'mph');
-    //         console.log('lat: ' + apiCurrentResponse.coord.lat);
-    //         console.log('lon: ' + apiCurrentResponse.coord.lon);
-    //         console.log(apiCurrentResponse);
-    //     });
-    // };
-
-    // function populateCurrentElements(event) {
-    // $.ajax({
-    //     url: apiLocationURL,
-    //     method: "GET",
-    // }).then(function(apiLocationResponse) {
-    //     var currentCity = apiLocationResponse.name;
-    //     var currentTemp = apiLocationResponse.main.temp + " F";
-    //     var currentIconCode = apiLocationResponse.weather.icon
-    //     var currentIconSource = apiIconsBaseString + currentIconCode + ".png";
-    //     var locationIconImageEl = "<img src=" + currentIconSource + ">"
-    //     $("#currentLoc").textContent(currentCity);
-    //     $("#currentTemp").textContent(currentTemp);
-    //     $("#locationIcon").prepend(locationIconImageEl);
-
-    //     // getUV(apiLocationResponse);
-    //     // createCurrentElements(apiCurrentResponse);
-
-    //     console.log(apiLocationResponse);
-    // });
-
-
     function getDefaultWeather(event) {
         $.ajax({
             url: apiDefaultURL,
@@ -112,11 +33,7 @@ var UV;
             apiUVlat = "lat=" + apiDefaultResponse.coord.lat; 
             apiUVlon = "&lon=" + apiDefaultResponse.coord.lon;
             apiUVURL = apiUVBaseString + apiUVlat + apiUVlon + apiKey;
-            console.log(apiUVlat);
-            console.log(apiUVlon);
-            console.log(apiUVURL);
             getUV(apiDefaultResponse);
-            console.log(apiDefaultResponse);
             apiFCastURL = proxy + apiFCastBaseString + apiCity + apiCountry + apiKey + apiUnits;
             getForecast(event);
             var currentDate = moment().format("MM/DD/YYYY");
@@ -137,16 +54,6 @@ var UV;
         });
     };
 
-
-
-
-    //api variables
-
-
-    //api calls
-
-
-
         //UV
         function getUV(apiDefaultResponse){
             var apiUVlat = 'lat=' + apiDefaultResponse.coord.lat;
@@ -157,28 +64,19 @@ var UV;
                 url: apiUVURL,
                 method: "GET",
             }).then(function(apiUVResponse) {
-                UV = 'UV index: ' + apiUVResponse.value
-                console.log(apiUVResponse);
-                console.log(UV);
+                UV = 'UV index: ' + apiUVResponse.value;
                 $("#currentWeatherUVData").text(UV);
             });
-            // return UV;
         };
 
     //forecast
-    // $("#fcasttestbtn").on("click", function(event) {
+
         function getForecast(event){
         $.ajax({
             url: apiFCastURL,
             method: "GET", 
             dataType: "JSON",
         }).then(function(apiFCastResponse) {
-        // showFCastData();
-
-        // function showFCastData(apiFCastResponse) {
-
-            console.log(apiFCastResponse);
-
             var nextDay = apiFCastResponse.list[4];
             var nextDayWeatherIcons = nextDay.weather[0];
             var nextDay2 = apiFCastResponse.list[12];
@@ -257,42 +155,56 @@ var UV;
             var nextDay4Data = [nextDay4.dt_txt, nextDay4.main.humidity, nextDay4.main.temp, nextDay4WeatherIcons.icon];
             var nextDay5Data = [nextDay5.dt_txt, nextDay5.main.humidity, nextDay5.main.temp, nextDay5WeatherIcons.icon];
 
-            var FCastResultsTest = [nextDayData, nextDay2Data, nextDay3Data, nextDay4Data, nextDay5Data];
-
-            console.log(FCastResultsTest);
-            
-            // var FCastResultsTest;
-        
-            // };
         });
 
 
 
     };
 
-    //need to create separate arrays for each date returned
-
-
-
-    // * Include a 5-Day Forecast below the current weather conditions. Each day for the 5-Day Forecast should display the following:
-
-    // * Date
-  
-    // * Icon image (visual representation of weather conditions)
-  
-    // * Temperature
-  
-    // * Humidity
-
-
-
-            // var apiIconCode = "10d"; //replace with icon code from api responses
-            // var apiIconURL = apiIconsBaseString + apiIconCode + ".png"; //may need to add @2x before .png
-            $(document).ready(function(){
-                defaultCity = localStorage.getItem("searchHistory");
-                defaultCity = JSON.parse(defaultCity);
-                defaultCity = defaultCity.lastCity;
+    $(document).ready(function(){
+                // defaultCity = localStorage.getItem("searchHistory");
+                // defaultCity = JSON.parse(defaultCity);
+                // defaultCity = defaultCity.lastCity;
+                defaultCity = "orlando";
                 apiDefaultURL = proxy + apiCurrentBaseString + defaultCity + apiCountry + apiKey + apiUnits;
                 getDefaultWeather(event);
 
 });
+
+// var currentLat;
+// var currentLon;
+// var apiLocationURL;
+
+// //Get current location and assign coordinate values to variable
+// navigator.geolocation.getCurrentPosition(function(position) {
+// currentLat = position.coords.latitude;
+// currentLon = position.coords.longitude;
+// apiLocationURL = proxy + apiLocationBaseString + "lat=" + currentLat + "&lon=" + currentLon;
+// console.log(currentLat);
+// console.log(currentLon);
+// console.log(apiLocationURL);
+// getCurrentLocationData(event);
+// });
+// function getCurrentLocationData(event) {
+//     console.log(currentLat);
+//     console.log(currentLon);
+//     console.log(apiLocationURL);
+//     $.ajax({
+//         url: apiLocationURL,
+//         method: "GET",
+//     }).then(function(apiLocationResponse) {
+//         var currentCity = apiLocationResponse.name;
+//         var currentTemp = apiLocationResponse.main.temp + " F";
+//         var currentIconCode = apiLocationResponse.weather.icon
+//         var currentIconSource = apiIconsBaseString + currentIconCode + ".png";
+//         var locationIconImageEl = "<img src=" + currentIconSource + ">"
+//         $("#currentLoc").textContent(currentCity);
+//         $("#currentTemp").textContent(currentTemp);
+//         $("#locationIcon").prepend(locationIconImageEl);
+
+//         // getUV(apiLocationResponse);
+//         // createCurrentElements(apiCurrentResponse);
+
+//         console.log(apiLocationResponse);
+//     });
+// };
